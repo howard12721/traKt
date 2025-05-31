151a9bb441566f57843f36462177cf393c749701
howard127 <kobun127@gmail.com>
2025-06-01 03:58:09 +0900

    Refactor build configuration to use version catalog for dependencies and add libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
-    kotlin("plugin.serialization") version "2.1.20"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.github.howard12721"
@@ -10,19 +10,24 @@ repositories {
     mavenCentral()
 }
 
-val ktorVersion = "2.3.10"
-
 dependencies {
-    implementation("io.ktor:ktor-client-core:${ktorVersion}")
-    implementation("io.ktor:ktor-client-cio:${ktorVersion}")
-    implementation("io.ktor:ktor-client-websockets:${ktorVersion}")
-    implementation("io.ktor:ktor-client-content-negotiation:${ktorVersion}")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("ch.qos.logback:logback-classic:1.5.18")
-    testImplementation(kotlin("test"))
+    // Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.websockets)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    // KotlinX
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+
+    // Logging
+    implementation(libs.logback.classic)
+
+    // Testing
+    testImplementation(libs.kotlin.test)
 }
 
 tasks.test {
