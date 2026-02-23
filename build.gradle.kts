4c2e3ab44f389de7d2b7fd2e9427d8116e4b30b0
howard127 <internal@xhw.jp>
2026-02-23 17:07:33 +0900

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,4 @@ plugins {
 }
 
 group = "jp.xhw"
-version = "1.0.0"
+version = "1.1.1"
--- trakt-bot/build.gradle.kts
+++ trakt-bot/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "jp.xhw"
-version = "1.0.0"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
