c5426a51180b306488442c30f453016211c6c976
howard127 <internal@xhw.jp>
2026-05-11 09:50:00 +0900

    feat: add type aliases for BotRuntime and UserRuntime

--- trakt-bot/src/main/kotlin/jp/xhw/trakt/bot/dsl/TraktDsl.kt
+++ trakt-bot/src/main/kotlin/jp/xhw/trakt/bot/dsl/TraktDsl.kt
@@ -1,4 +1,14 @@
 package jp.xhw.trakt.bot.dsl
 
+import jp.xhw.trakt.bot.context.bot.BotContext
+import jp.xhw.trakt.bot.context.user.UserContext
+import jp.xhw.trakt.bot.infrastructure.runtime.Runtime
+import jp.xhw.trakt.bot.model.BotEvent
+import jp.xhw.trakt.bot.model.UserEvent
+
 @DslMarker
 annotation class TraktDsl
+
+typealias BotRuntime = Runtime<BotContext, BotEvent>
+
+typealias UserRuntime = Runtime<UserContext, UserEvent>
